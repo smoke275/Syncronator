25b28ffbe9dd202599d02e3a1964d8eca3268aaa
Shashwata Mandal <shashwata275@gmail.com>
2018-02-12 23:26:00 -0500

    Added System tray setup for worker processes

--- src/main/java/views/ApplicationSystemTray.java
+++ src/main/java/views/ApplicationSystemTray.java
@@ -1,5 +1,7 @@
 package views;
 
+import workers.Worker;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +17,6 @@ import java.util.ResourceBundle;
 
 public class ApplicationSystemTray {
 
-    String MESSAGE_HEADER = "Mindfire SystemTray Example";
     TrayIcon processTrayIcon = null;
 
     private static ApplicationSystemTray applicationSystemTray;
@@ -67,40 +68,20 @@ public class ApplicationSystemTray {
         final SystemTray tray = SystemTray.getSystemTray();
 
         // Create a popup menu components
-        MenuItem aboutItem = new MenuItem("About");
-        MenuItem launchApplication = new MenuItem("Launch Application");
+        MenuItem aboutItem = new MenuItem(labels.getString("about_label"));
+        MenuItem launchApplication = new MenuItem(labels.getString("launch_application_label"));
 
-        CheckboxMenuItem autoSizeCheckBox = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem toolTipCheckBox = new CheckboxMenuItem("Set tooltip");
 
-        Menu displayMenu = new Menu("Display");
-
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
-
-        MenuItem exitItem = new MenuItem("Exit");
+        MenuItem exitItem = new MenuItem(labels.getString("exit_label"));
 
         // Add components to popup menu
         popup.add(launchApplication);
         popup.add(aboutItem);
         popup.addSeparator();
-        popup.add(autoSizeCheckBox);
-        popup.add(toolTipCheckBox);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
         popup.add(exitItem);
 
         trayIcon.setPopupMenu(popup);
 
-        // Setting toolTipCheck and autoSizeCheckBox state as true
-        toolTipCheckBox.setState(true);
-        autoSizeCheckBox.setState(true);
         trayIcon.setImageAutoSize(true);
 
         try {
@@ -111,71 +92,14 @@ public class ApplicationSystemTray {
         }
 
         // Add listener to trayIcon.
-        trayIcon.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,
-                        "This dialog box is run from System Tray");
-            }
-        });
+        trayIcon.addActionListener(e -> FileExplorer.invokeFileManager());
 
         // Add listener to aboutItem.
-        aboutItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,
-                        "This dialog box is run from the About menu item");
-            }
-        });
+        aboutItem.addActionListener(e -> JOptionPane.showMessageDialog(null,
+                labels.getString("about_message")));
 
-        launchApplication.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                FileExplorer.invokeFileManager();
-            }
-        });
+        launchApplication.addActionListener(e -> FileExplorer.invokeFileManager());
 
-        // Add listener to autoSizeCheckBox.
-        autoSizeCheckBox.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                int autoSizeCheckBoxId = e.getStateChange();
-                if (autoSizeCheckBoxId == ItemEvent.SELECTED) {
-                    trayIcon.setImageAutoSize(true);
-                } else {
-                    trayIcon.setImageAutoSize(false);
-                }
-            }
-        });
-
-        // Add listener to toolTipCheckBox.
-        toolTipCheckBox.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                int toolTipCheckBoxId = e.getStateChange();
-                if (toolTipCheckBoxId == ItemEvent.SELECTED) {
-                    trayIcon.setToolTip(labels.getString("system_tray_tooltip"));
-                } else {
-                    trayIcon.setToolTip(null);
-                }
-            }
-        });
-
-        // Create listener for Display menu items.
-        ActionListener listener = e -> {
-            MenuItem item = (MenuItem) e.getSource();
-            System.out.println(item.getLabel());
-            if ("Error".equals(item.getLabel())) {
-                trayIcon.displayMessage(MESSAGE_HEADER,"This is an error message",TrayIcon.MessageType.ERROR);
-            } else if ("Warning".equals(item.getLabel())) {
-                trayIcon.displayMessage(MESSAGE_HEADER,"This is a warning message",TrayIcon.MessageType.WARNING);
-            } else if ("Info".equals(item.getLabel())) {
-                trayIcon.displayMessage(MESSAGE_HEADER,"This is an info message",TrayIcon.MessageType.INFO);
-            } else if ("None".equals(item.getLabel())) {
-                trayIcon.displayMessage(MESSAGE_HEADER,"This is an ordinary message",TrayIcon.MessageType.NONE);
-            }
-        };
-
-        // Add listeners to Display menu items.
-        errorItem.addActionListener(listener);
-        warningItem.addActionListener(listener);
-        infoItem.addActionListener(listener);
-        noneItem.addActionListener(listener);
 
         // Add listener to exitItem.
         exitItem.addActionListener(e -> {
@@ -184,29 +108,9 @@ public class ApplicationSystemTray {
         });
     }
 
-    /**
-     * This method will start a thread that will
-     * show a popup message from the system tray after every 10 secs.
-     */
-
     private void startProcess() {
 
-        Thread thread = new Thread(() -> {
-            int i = 0;
-            while (true) {
-                i++;
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                processTrayIcon.displayMessage("Mindfire Process Message",
-                        "This is message number " + i,
-                        TrayIcon.MessageType.INFO);
-            }
-
-        });
-
+        Thread thread = new Thread(() -> new Worker());
         thread.start();
     }
 }
